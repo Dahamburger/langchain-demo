b4609882d69a7916399fd86c9e4bd17d2ef6bac8
Dahamburger <2091994347@qq.com>
2026-06-12 15:04:05 +0800

    demo文件

---
+++ helloworld/langchain1.0.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+QWEN_API_KEY = os.getenv("QWEN_API_KEY")
+QWEN_BASE_URL = os.getenv("QWEN_BASE_URL")
+# print(QWEN_API_KEY, QWEN_BASE_URL)
+
+llm = init_chat_model(
+    model="qwen-plus",
+    model_provider="openai",
+    api_key=QWEN_API_KEY,
+    base_url=QWEN_BASE_URL
+)
+response = llm.invoke("你好")
+print(response)
